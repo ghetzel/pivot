08b817dbe77eaca08ae48673c996898a28a0d033
Gary Hetzel <garyhetzel@gmail.com>
2016-07-30 00:32:39 -0400

    Added index page

--- static/js/app.js
+++ static/js/app.js
@@ -15,7 +15,14 @@ var BackendController = Stapes.subclass({
 });
 
 var Pivot = Stapes.subclass({
-    constructor: function(){
+    constructor: function(options){
+        this.site = {
+            title: 'Pivot Manager',
+            brand: 'Pivot',
+        };
+
+        this.extend(options || {});
+
         rivets.configure({
              templateDelimiters: ['{{', '}}'],
         });
@@ -36,15 +43,36 @@ var Pivot = Stapes.subclass({
             return JSON.stringify(value, null, 2);
         };
 
+        rivets.formatters.values = function(value){
+            var values = [];
+
+            switch(typeof value) {
+            case 'object':
+                $.each(value, function(_, v){
+                    values.push(v);
+                });
+
+                break;
+
+            case 'array':
+                values = value;
+                break;
+
+            default:
+                values = [value];
+                break;
+            }
+
+            return values;
+        };
+
         this.backends = {};
         this._oldpath = null;
-
-        this.site = {
-            title: 'Pivot Manager',
-            brand: 'Pivot',
-        };
     },
 
+    // Setup page bindings and routes and perform the initial data load, then
+    // show everything.
+    //
     run: function(){
         this._boundFullPage = rivets.bind($('html'), this);
 
@@ -53,20 +81,30 @@ var Pivot = Stapes.subclass({
                 this.chpage('backend',
                     new BackendController(this.backends[id], this));
             }.bind(this),
+
+            '/': function(){
+                this.chpage('index');
+            }.bind(this),
         });
 
         this.load(function(){
-            console.debug('Load complete');
+            this.chpage('index');
             this._router.init();
             $('body').removeAttr('style');
             $('title').text(this.site.title);
         }.bind(this));
     },
 
+    // Load the API data the site depends on
+    //
     load: function(callback){
         this._loadCallback = callback;
+
+        // should equal the number of $.ajax calls that happen below,
+        // and each call (when successful) should call _checkLoad()
         this._loadOutstanding = 1;
 
+        // load details about all backends
         $.ajax({
             url: '/api/backends',
         }).success(function(data){
@@ -75,12 +113,10 @@ var Pivot = Stapes.subclass({
         });
     },
 
+    // Attempt to load the content of the named template into the container pointed at by viewTarget
+    //
     chpage: function(template, controller) {
-        console.debug('chpage', template, controller);
-
-        $('#content').load('/views/'+template+'.html', null, function(content, status){
-            console.debug('Loaded', template, arguments);
-
+        $(this.viewTarget).load('/views/'+template+'.html', null, function(content, status){
             switch(status){
             case 'success':
                 if(controller){
@@ -88,21 +124,24 @@ var Pivot = Stapes.subclass({
                         rivets.unbind(this._boundContent);
                     }
 
-                    rivets.bind($('#content'), controller);
+                    rivets.bind($(this.viewTarget), controller);
                 }
 
                 break;
 
             default:
-                $('#content').load('/views/error.html', null, function(content, status){
+                $(this.viewTarget).load('/views/error.html', null, function(content, status){
                     if(status == 'error'){
-                        $('#content').text('Failed to load template "'+template+'". Additionally, an error page is not configured.');
+                        $(this.viewTarget).text('Failed to load template "'+template+'". Additionally, an error page is not configured.');
                     }
                 });
             }
         }.bind(this));
     },
 
+    // If a load callback was given, decrement the outstanding calls counter
+    // and fire the callback if the counter is <= 0
+    //
     _checkLoad: function(){
         if(typeof this._loadCallback === 'function') {
             this._loadOutstanding--;
@@ -116,6 +155,9 @@ var Pivot = Stapes.subclass({
 });
 
 $(document).ready(function(){
-    window.pivot = new Pivot();
+    window.pivot = new Pivot({
+        viewTarget: '#content',
+    });
+
     pivot.run();
 });
