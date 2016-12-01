764b646042b8413d557b8273a2a426ab6a7d149d
Gary Hetzel <garyhetzel@gmail.com>
2016-12-01 16:12:05 -0500

    Added pivot.NewDatabase

--- pivot.go
+++ pivot.go
@@ -1,9 +1,27 @@
 package pivot
 
 import (
+	"github.com/ghetzel/pivot/backends"
+	"github.com/ghetzel/pivot/dal"
 	"github.com/op/go-logging"
 	"time"
 )
 
 var log = logging.MustGetLogger(`pivot`)
 var MonitorCheckInterval = time.Duration(10) * time.Second
+
+func NewDatabase(connection string) (backends.Backend, error) {
+	if cs, err := dal.ParseConnectionString(connection); err == nil {
+		if backend, err := backends.MakeBackend(cs); err == nil {
+			if err := backend.Initialize(); err == nil {
+				return backend, nil
+			} else {
+				return nil, err
+			}
+		} else {
+			return nil, err
+		}
+	} else {
+		return nil, err
+	}
+}
