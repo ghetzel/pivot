de5069101269265d2cb136097955c37eb0b6e699
Gary Hetzel <garyhetzel@gmail.com>
2017-12-21 19:11:03 -0500

    Version bump

--- util/constants.go
+++ util/constants.go
@@ -2,4 +2,4 @@ package util
 
 const ApplicationName = `pivot`
 const ApplicationSummary = `an extensible database abstraction service`
-const ApplicationVersion = `1.0.0`
+const ApplicationVersion = `1.1.0`
